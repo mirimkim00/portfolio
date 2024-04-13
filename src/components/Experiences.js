79cefdd032a2e51b36196e7fed15699c53ff1ace
Mirim Kim <mirimkim00@gmail.com>
2024-04-13 16:11:05 -0400

    completed experiences

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,9 +1,9 @@
 export default function Expereinces() {
     return (
         <>
-            <h3 className="ttl">Experiences</h3>
-            <div className="experiences" style={{ display: "flex" }}>
-                <div className="job ex">
+            <h3 className="ttl allerta">Experiences</h3>
+            <div className="experiences">
+                <div className="job ex asap">
                     <div>
                         <h5>Software Developer</h5>
                         <p>
@@ -12,7 +12,7 @@ export default function Expereinces() {
                             Alpha Labs, North York, Canada
                         </p>
                     </div>
-                    <div>
+                    <div style={{marginTop: '110px'}}>
                         <h5>Mobile Application Developer</h5>
                         <p>
                             Volunteer<br />
@@ -28,7 +28,7 @@ export default function Expereinces() {
                         <div class="vl"></div>
                     </div>
                     <p className="dot" style={{ backgroundColor: '#FFC738' }}></p>
-                    <div style={{ display: "ruby-text"}}>
+                    <div style={{ display: "ruby-text" }}>
                         <div class="vl"></div>
                     </div>
                     <p className="dot" style={{ backgroundColor: '#DD3636' }}></p>
@@ -37,24 +37,41 @@ export default function Expereinces() {
                     </div>
                 </div>
 
-                <div className="jd ex">
-                    <ul>
-                        <li>
-                            Addressed website errors and deployed updates swiftly in cross-functional team using C# and .NET.
-                        </li>
+                <div className="jd ex asap">
+                    <div className="jdContent">
+                        <ul>
+                            <li>
+                                Addressed website errors and deployed updates swiftly in cross-functional team using C# and .NET.
+                            </li>
 
-                        <li>
-                            Developed a controller to support additional functionality and established a corresponding database.
-                        </li>
-                        <li>
-                            Created test scripts using JUnit and Selenium to thoroughly conduct unit and integration testing, both automatically and manually.
-                        </li>
-                        <li>
-                            Authored structured documents to organize the project workflow and user guide documents providing step-by-step instructions.
-                        </li>
-                    </ul>
+                            <li>
+                                Developed a controller to support additional functionality and established a corresponding database.
+                            </li>
+                            <li>
+                                Created test scripts using JUnit and Selenium to thoroughly conduct unit and integration testing, both automatically and manually.
+                            </li>
+                            <li>
+                                Authored structured documents to organize the project workflow and user guide documents providing step-by-step instructions.
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="jdContent">
+                        <ul>
+                            <li>
+                                Developed a mobile application called SportManager for targeted end users on both iOS and Android platforms using Flutter and Firebase, collaborating closely with clients throughout the process.
+                            </li>
+
+                            <li>
+                                Oversaw sprints and generated documents and charts, including Gantt charts and Burndown charts, to facilitate the management of the overall project process.
+                            </li>
+                            <li>
+                                Automatically assigned tasks to team members and imposed penalties.
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
