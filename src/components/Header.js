c3fde64615097ea24d4b60f3f41e8eb7db04262a
Mirim Kim <mirimkim00@gmail.com>
2024-04-28 15:14:35 -0400

    mobile navbar disappears on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,38 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from 'react-scroll';
+import { useState } from 'react';
 
 export default function Header() {
+    const [show, setShow] = useState(false);
+    const toggleOffcanvas = () => {
+        setShow(!show);
+    };
+
     return (
         <header>
             <Navbar key='md' expand='md' className="navbar-expand-lg mb-3">
                 <Container fluid>
                     <Navbar.Brand id='mirim' className='me-auto asap' href="#">MIRIM</Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} onClick={() => { setShow(true) }} />
                     <Navbar.Offcanvas
                         id={`offcanvasNavbar-expand-md`}
                         aria-labelledby={`offcanvasNavbarLabel-expand-md`}
                         placement="end"
+                        show={show}
                     >
-                        <Offcanvas.Header closeButton>
+                        <Offcanvas.Header closeButton onClick={() => { setShow(false) }}>
                             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`} className='asap' style={{ fontSize: '25px', fontWeight: 600 }}>
                                 MIRIM
                             </Offcanvas.Title>
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                             <Nav className="justify-content-end flex-grow-1 pe-3 navLinks">
-                                <Link to='about' duration={500} offset={-80}>ABOUT</Link>
-                                <Link to='experiences' duration={500} offset={-80}>EXPERIENCES</Link>
-                                <Link to='skills' duration={500} offset={-80}>SKILLS</Link>
-                                <Link to='projects' duration={500} offset={-80}>PROJECTS</Link>
-                                <Link to='contact' duration={500} offset={-80}>CONTACT</Link>
+                                <Link to='about' duration={500} offset={-80} onClick={toggleOffcanvas}>ABOUT</Link>
+                                <Link to='experiences' duration={500} offset={-80} onClick={toggleOffcanvas}>EXPERIENCES</Link>
+                                <Link to='skills' duration={500} offset={-80} onClick={toggleOffcanvas}>SKILLS</Link>
+                                <Link to='projects' duration={500} offset={-80} onClick={toggleOffcanvas}>PROJECTS</Link>
+                                <Link to='contact' duration={500} offset={-80} onClick={toggleOffcanvas}>CONTACT</Link>
                                 <button id='resumeBtn'>
                                     RESUME
                                 </button>
