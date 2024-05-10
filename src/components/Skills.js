c500adcb6aeade617d46ffd63cc812e51aa8ba54
Mirim Kim <mirimkim00@gmail.com>
2024-05-10 11:55:47 -0400

    completed skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,57 @@
 import html5 from '../img/html5.png';
 import css3 from '../img/css3.png';
-import js from '../img/js.png';
+import js from '../img/javascript.png';
+import react from '../img/react.png';
 import nodejs from '../img/nodejs.png';
+import aspnet from '../img/aspnet.png';
+import oracle from '../img/oracle.png';
+// import mongoDB from '../img/mongoDB.png';
+import firebase from '../img/firebase.png';
+import aws from '../img/aws.png';
+import docker from '../img/docker.png';
+import git from '../img/git.png';
+import github from '../img/github.png';
+import trello from '../img/trello.png';
+import figma from '../img/figma.png';
+// import postgreSQL from '../img/postgreSQL.png';
 
 export default function Skills() {
     return (
         <section id="skills">
             <h2 className="ttl allerta">Skills</h2>
             <div className="skills">
-
-
                 <div className="skillDiv" style={{ '--backgroundImage': `var(--frontend)` }}>
                     {/* <a href="https://www.freepik.com/free-vector/frontend-development-concept-website-interface-design-improvement-web-page-programming-coding-testing-it-profession-isolated-flat-vector-illustration_28158111.htm">Image by vector4stock</a> on Freepik */}
                     <div className="intro asap">
                         <h4>
                             Front End
                         </h4>
-                        <p>
-                            laksdjfawej foe jf oeirjfwqejfkfdjkal jiwehf kalsfd
-                        </p>
+                        <div className="skillIcon frontend">
+                            <div>
+                                <img src={html5} alt='HTML5' />
+                                <p>
+                                    HTML5
+                                </p>
+                            </div>
+                            <div>
+                                <img src={css3} alt='CSS3' />
+                                <p>
+                                    CSS3
+                                </p>
+                            </div>
+                            <div>
+                                <img src={js} alt='JavaScript' />
+                                <p>
+                                    JavaScript
+                                </p>
+                            </div>
+                            <div>
+                                <img src={react} alt='React' />
+                                <p>
+                                    React
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -28,9 +61,44 @@ export default function Skills() {
                         <h4>
                             Back End
                         </h4>
-                        <p>
-                            laksdjfawej foe jf oeirjfwqejfkfdjkal jiwehf kalsfd
-                        </p>
+                        <div className="skillIcon backend">
+                            <div>
+                                <img src={nodejs} alt='Node.js' />
+                                <p>
+                                    Node.js
+                                </p>
+                            </div>
+                            <div>
+                                <img src={aspnet} alt='Node.js' />
+                                <p>
+                                    ASP.NET
+                                </p>
+                            </div>
+                            {/* <div>
+                                <img src={mongoDB} alt='MongoDB' />
+                                <p>
+                                    MongoDB
+                                </p>
+                            </div> */}
+                            <div>
+                                <img src={oracle} alt='Oracle' />
+                                <p>
+                                    Oracle
+                                </p>
+                            </div>
+                            <div>
+                                <img src={firebase} alt='Firebase' />
+                                <p>
+                                    Firebase
+                                </p>
+                            </div>
+                            {/* <div>
+                                <img src={postgreSQL} alt='PostgreSQL' />
+                                <p>
+                                    PostgreSQL
+                                </p>
+                            </div> */}
+                        </div>
                     </div>
                 </div>
 
@@ -40,24 +108,57 @@ export default function Skills() {
                         <h4>
                             Cloud Computing
                         </h4>
-                        <p>
-                            laksdjfawej foe jf oeirjfwqejfkfdjkal jiwehf kalsfd
-                        </p>
+                        <div className="skillIcon cloud">
+                            <div>
+                                <img src={aws} alt='AWS' />
+                                <p className='aws'>
+                                    AWS
+                                </p>
+                            </div>
+                            <div>
+                                <img src={docker} alt='Docker' />
+                                <p>
+                                    Docker
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="skillDiv" style={{ '--backgroundImage': `var(--pmTools)` }}>
                     {/* <a href="https://www.freepik.com/free-vector/hand-drawn-flat-design-devops-illustration_25559495.htm#fromView=search&page=1&position=5&uuid=86639522-1646-46e6-926c-8e7be7aedb0d">Image by freepik</a> */}
                     <div className="intro asap">
-                        <h4>
+                        <h4 style={{ transform: `translateY(-15px)` }}>
                             Project Management Tools & Design
                         </h4>
-                        <p>
-                            laksdjfawej foe jf oeirjfwqejfkfdjkal jiwehf kalsfd
-                        </p>
+                        <div className="skillIcon tools">
+                            <div>
+                                <img src={git} alt='Git' />
+                                <p>
+                                    Git
+                                </p>
+                            </div>
+                            <div>
+                                <img src={github} alt='GitHub' />
+                                <p>
+                                    GitHub
+                                </p>
+                            </div>
+                            <div>
+                                <img src={trello} alt='Trello' />
+                                <p>
+                                    Trello
+                                </p>
+                            </div>
+                            <div>
+                                <img src={figma} alt='Figma' />
+                                <p>
+                                    Figma
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
 
             </div>
         </section >
